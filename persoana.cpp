0129d3cd7a8fadaf11c109883443567c186e9296
paul <mihaicutapg@gmail.com>
2024-03-26 16:42:46 +0200

    Refactor Persoana class constructors and add default constructor

--- persoana.cpp
+++ persoana.cpp
@@ -1,6 +1,21 @@
-#include "persoana.h"
 #include <iostream>
 #include <string>
+#include "persoana.h"
 
 using namespace std;
+
+Persoana::Persoana() {
+    nume = "";
+    prenume = "";
+    CNP = "";
+    student = false;
+}
+
+Persoana::Persoana(string new_nume, string new_prenume, string new_CNP, bool new_student) {
+    nume = new_nume;
+    prenume = new_prenume;
+    CNP = new_CNP;
+    student = new_student;
+}
+
 
